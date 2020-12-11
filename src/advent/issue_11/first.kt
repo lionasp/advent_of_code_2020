79bae198592c6cb80d4cb60c2c655051945a5ab0
lionasp <lion.programmer@gmail.com>
2020-12-11 19:37:06 +0300

    day 11 issue 1

---
+++ src/advent/issue_11/first.kt
@@ -0,0 +1,85 @@
+package advent.issue_11
+
+import java.io.File
+
+
+fun calculateOccupiedCount(field: List<String>, i: Int, j: Int): Int {
+    var occupiedAround = 0
+    val kFrom = if (i - 1 > 0) i - 1 else 0
+    val kTo = if (i + 1 > field.lastIndex) field[i].lastIndex else i + 1
+    val zFrom = if (j - 1 > 0) j - 1 else 0
+    val zTo = if (j + 1 > field[i].lastIndex) field.lastIndex else j + 1
+    for (k in kFrom..kTo) {
+        for (z in zFrom..zTo) {
+            if (k == i && z == j) {
+                continue
+            }
+
+            if (field[k][z] == '#') {
+                occupiedAround++
+            }
+        }
+    }
+    return occupiedAround
+}
+
+fun getTotalOccupied(field: List<String>): Int {
+    var counter = 0
+    for (i in field) {
+        for (j in i) {
+            if (j == '#') {
+                counter++
+            }
+        }
+    }
+    return counter
+}
+
+fun replaceStr(s: String, position: Int, newChar: Char): String {
+    return s.substring(0, position) + newChar + s.substring(position + 1)
+}
+
+fun iteration(currentState: MutableList<String>): MutableList<String> {
+    val newState = currentState.toMutableList()
+    for (i in currentState.indices) {
+        for (j in currentState[i].indices) {
+            if (currentState[i][j] != '.') {
+                val occupiedAround = calculateOccupiedCount(currentState, i, j)
+                if (currentState[i][j] == 'L' && occupiedAround == 0) {
+                    newState[i] = replaceStr(newState[i], j, '#')
+                } else if (currentState[i][j] == '#' && occupiedAround >= 4) {
+                    newState[i] = replaceStr(newState[i], j, 'L')
+                }
+            }
+        }
+    }
+    return newState
+}
+
+
+fun printField(field: List<String>) {
+    for (s in field) {
+        println(s)
+    }
+    println("------------")
+}
+
+fun main() {
+    val fileName = "src/advent/issue_11/input.txt"
+    var state = File(fileName).readLines().toMutableList()
+    var lastOccupied = 0
+
+    while (true) {
+
+        state = iteration(state)
+        //  printField(state)
+        val total = getTotalOccupied(state)
+
+        if (total == lastOccupied) {
+            println(total)
+            break
+        }
+
+        lastOccupied = total
+    }
+}
